3f66ab5e2343273ff1b4f189ba1f89522c148198
992999 <992999@LONW000I007486.bnppi.priv>
2017-05-30 10:08:37 +0100

    check sync

--- sockets/src/main/java/sockets/Example.java
+++ sockets/src/main/java/sockets/Example.java
@@ -7,50 +7,50 @@ import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
- 
+
 import com.google.common.collect.ImmutableList;
 import com.google.common.jimfs.Configuration;
 import com.google.common.jimfs.Jimfs;
- 
+
 public class Example {
- 
+
 	public static void main(final String[] args) throws IOException {
-		System.out.println("Example 1: Creating, copying, reading files and directories");
+		System.out
+				.println("Example 1: Creating, copying, reading files and directories");
 		FileSystem fs = Jimfs.newFileSystem(Configuration.unix());
 		Path data = fs.getPath("/data");
 		Files.createDirectory(data);
- 
+
 		Path data2 = fs.getPath("/data/data2");
 		Files.createDirectory(data2);
-		
+
 		Path hello = data.resolve("test.txt"); // /data/test.txt
 		Files.write(hello, ImmutableList.of("hello"), StandardCharsets.UTF_8);
- 
+
 		Path csv = data.resolve("data.csv"); // /data/data.csv
-		Files.write(csv, ImmutableList.of("test1,test2\ntest3,test4"), StandardCharsets.UTF_8);
- 
+		Files.write(csv, ImmutableList.of("test1,test2\ntest3,test4"),
+				StandardCharsets.UTF_8);
+
 		InputStream istream = Example.class.getResourceAsStream("/book.xml");
 		Path xml = data.resolve("book.xml"); // /data/book.xml
 		Files.copy(istream, xml, StandardCopyOption.REPLACE_EXISTING);
- 
-		
-		
-		Files.list(data).forEach(file -> {
-			try {
-				if ( ! Files.isDirectory(file)) {
-					System.out.println(String.format("%s (%db)", file, Files.readAllBytes(file).length));
-				} else {
-					
-					System.out.println(String.format("%s ", file));
-				}
-				
-				
-				
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		});
-		
-	}	
-		
+
+		Files.list(data).forEach(
+				file -> {
+					try {
+						if (!Files.isDirectory(file)) {
+							System.out.println(String.format("%s (%db)", file,
+									Files.readAllBytes(file).length));
+						} else {
+
+							System.out.println(String.format("%s ", file));
+						}
+
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				});
+
+	}
+
 }
